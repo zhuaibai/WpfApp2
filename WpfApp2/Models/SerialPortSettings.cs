49413c56c6dfcce66bdd96836a36e215557cc3fc
罗福全 <778393374@qq.com>
2025-05-06 20:27:15 +0800

    添加项目文件。

---
+++ WpfApp2/Models/SerialPortSettings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace WpfApp2.Models
+{
+    [XmlRoot("SerialPortSettings")]
+    public class SerialPortSettings
+    {
+        [XmlElement("PortName")]
+        public string PortName { get; set; } = "COM1";
+
+        [XmlElement("BaudRate")]
+        public int BaudRate { get; set; } = 2400;
+
+        [XmlElement("DataBits")]
+        public int DataBits { get; set; } = 8;
+
+        [XmlElement("StopBits")]
+        public StopBits StopBits { get; set; } = StopBits.One;
+
+        [XmlElement("Parity")]
+        public Parity Parity { get; set; }=Parity.None;
+    }
+}
+
---
+++ WpfApp2/ViewModels/MainWindowVM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using WpfApp2.UserControls;
+
+namespace WpfApp2.ViewModels
+{
+    public class MainWindowVM:BaseViewModel
+    {
+		public MainWindowVM()
+		{
+
+		}
+
+		private UserControl _ContentControl;
+
+		public UserControl ContentControl
+		{
+			get {
+				if(_ContentControl == null)
+				{
+					_ContentControl = new SetProtolViewUC();
+				}
+				return _ContentControl; }
+			set
+			{
+				_ContentControl = value;
+				this.RaiseProperChanged(nameof(ContentControl));
+			}
+		}
+
+	}
+}
